62dab3a31abfdd70e7fe020e7fb4fb53a6a57795
Roma <roma.li0071@gmail.com>
2024-06-13 21:54:07 +0500

    Добавил DivideView, которое возвращает частное двух чисел при POST запросе, всегда делит a на b, при делении на ноль возвращает 400 ошибку

--- drf_hw_project/api_calculator/urls.py
+++ drf_hw_project/api_calculator/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('v1/add/', views.AddView.as_view()),
     path('v1/multiply/', views.MultiplyView.as_view()),
     path('v1/subtract/', views.SubtractView.as_view()),
+    path('v1/divide/', views.DivideView.as_view()),
 ]
--- drf_hw_project/api_calculator/views.py
+++ drf_hw_project/api_calculator/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -38,6 +39,25 @@ class SubtractView(APIView):
         a, b = request.data['a'], request.data['b']
         if isinstance(a, int) and isinstance(b, int):
             answer = {"answer": a - b}
+        else:
+            answer = {"error": "incorrect input"}
+
+        return Response(answer)
+
+
+class DivideView(APIView):
+    def get(self, request):
+        return Response({"a": "enter a number", "b": "enter a number", "returns": "quotient"})
+
+    def post(self, request):
+        a, b = request.data['a'], request.data['b']
+
+        if b == 0:
+            error = {"error": "Division by zero!"}
+            return Response(error, status=status.HTTP_400_BAD_REQUEST, )
+
+        if isinstance(a, int) and isinstance(b, int):
+            answer = {"answer": a / b}
         else:
             answer = {"error": "incorrect input"}
 
