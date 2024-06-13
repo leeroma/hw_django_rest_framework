fd2549cfb87204ef10dfd374e0e8a0fe561edd8e
Roma <roma.li0071@gmail.com>
2024-06-13 21:34:17 +0500

    Добавил MultiplyView, которое возвращает произведение двух чисел при POST запросе

--- drf_hw_project/api_calculator/urls.py
+++ drf_hw_project/api_calculator/urls.py
@@ -4,4 +4,5 @@ from api_calculator import views
 
 urlpatterns = [
     path('v1/add/', views.AddView.as_view()),
+    path('v1/multiply/', views.MultiplyView.as_view()),
 ]
--- drf_hw_project/api_calculator/views.py
+++ drf_hw_project/api_calculator/views.py
@@ -4,10 +4,27 @@ from rest_framework.views import APIView
 
 class AddView(APIView):
     def get(self, request):
-        return Response({"a": "enter a number", "b": "enter a number"})
+        return Response({"a": "enter a number", "b": "enter a number", "returns": "sum"})
 
     def post(self, request):
         a, b = request.data['a'], request.data['b']
-        answer = {"answer": a + b}
+        if isinstance(a, int) and isinstance(b, int):
+            answer = {"answer": a + b}
+        else:
+            answer = {"error": "incorrect input"}
+
         return Response(answer)
 
+
+class MultiplyView(APIView):
+    def get(self, request):
+        return Response({"a": "enter a number", "b": "enter a number", "returns": "product"})
+
+    def post(self, request):
+        a, b = request.data['a'], request.data['b']
+        if isinstance(a, int) and isinstance(b, int):
+            answer = {"answer": a * b}
+        else:
+            answer = {"error": "incorrect input"}
+
+        return Response(answer)
